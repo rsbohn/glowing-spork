94952172f7f11a05f968728c6abbda9024eb67d9
Randall Bohn <rsbohn@gmail.com>
2021-11-06 08:13:01 -0600

    adding danfo.js

--- spork.js
+++ spork.js
@@ -13,9 +13,18 @@ let paintBrick = function (column, row, color) {
     gameboard.fillRect(ox, oy, brick_width - 1, brick_height - 1);
 }
 broken = 'black';
-colors = ['pink', 'aqua', jade="#00A36C", amber="#FFC300"]
-for (x = 0; x < 16; x++) {
-    for (y = 0; y < 4; y++) {
-        paintBrick(x, y, colors[y]);
+colors = ['pink', 'aqua', jade = "#00A36C", amber = "#FFC300"]
+for (n = 0; n < 64; n++) {
+    x = n % 16;
+    y = Math.floor(n / 16);
+    paintBrick(x, y, colors[y])
+}
+
+let jumble = function(color) {
+    for (n = 0; n < 64; n++) {
+        x = n % 16; y = Math.floor(n / 16);
+        if (Math.random() < 0.3) {
+            paintBrick(x,y,color);
+        }
     }
 }
